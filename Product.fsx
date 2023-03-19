76ff1a452d4abff4d7c1044da148bf6097b777f4
MariAsenova <mariaasenova22@gmail.com>
2023-03-19 20:03:37 +0000

    refactor product and add helper function to calculate price with VAT

--- Product.fsx
+++ Product.fsx
@@ -1,51 +1,71 @@
-module Product
+module ProductM
+open System
 
-// types of drinks using discriminated union
+// Helper function to calculate price with VAT
+let applyVAT (percentage : int) (price : float) = 
+    price * (1.0 + Convert.ToDouble(percentage) / 100.0)
 
-type DrinkSize =
-    | Small
-    | Medium
-    | Large
+// Types for different sizes and drink varieties
+type Size = Small | Medium | Large
+type CoffeeType = Americano | Latte | Espresso
+type TeaType = Green | Black | Jasmine
+type JuiceType = Apple | Orange | Grepefruit
 
-type CoffeeType =
-    | Americano
-    | Espresso
-    | Latte
+// Record types for drinks with their size and type
+type CoffeeRecord = {size: Size; coffeeType: CoffeeType}
+type TeaRecord = {size: Size; teaType: TeaType}
+type JuiceRecord = {size: Size; juiceType: JuiceType}
 
-type TeaType =
-    | Green
-    | Black
-    | Jasmine
+// Union type for different drink categories
+type DrinkBase = Coffee of CoffeeRecord | Tea of TeaRecord | Juice of JuiceRecord
 
+// Functions to calculate the price based on the size of each drink type
+let priceSizeCoffee (coffee : CoffeeRecord) = 
+    match coffee.size with 
+    | Small -> 8.0
+    | Medium -> 10.0
+    | Large -> 12.0
 
-type JuiceType =
-    | Orange
-    | Apple
-    | Grepefruit
+let priceSizeTea (tea : TeaRecord) = 
+    match tea.size with 
+    | Small -> 6.0
+    | Medium -> 8.0
+    | Large -> 10.0
 
-type CoffeeDrink = { drinkSize:DrinkSize; coffeeType:CoffeeType }
-type TeaDrink = {drinkSize:DrinkSize; teaType: TeaType}
-type JuiceDrink = {drinkSize:DrinkSize; juiceType: JuiceType}
+let priceSizeJuice (juice : JuiceRecord) = 
+    match juice.size with 
+    | Small -> 10.0
+    | Medium -> 12.0
+    | Large -> 14.0
 
-type DrinkType =
-    | CoffeeDrink
-    | TeaDrink
-    | JuiceDrink
+// Function to calculate the price of a drink matching type
+let priceDrink (drink : DrinkBase) = 
+    match drink with 
+    | Coffee(coffeeRecord) -> priceSizeCoffee(coffeeRecord) |> applyVAT 25
+    | Tea(teaRecord) -> priceSizeTea(teaRecord)
+    | Juice(juiceRecord) -> priceSizeJuice(juiceRecord)
 
-type Drink =
-    { DrinkType : DrinkType
-      DrinkSize : DrinkSize }
+// Union type for products, which can be drinks with quantity
+type Product = 
+    | Drink of DrinkBase * qty: int 
 
-let priceForDrink (drink: Drink) =
-    match drink with
-    | { DrinkType = CoffeeDrink; DrinkSize = Small } -> 8
-    | { DrinkType = CoffeeDrink; DrinkSize = Medium } -> 10
-    | { DrinkType = CoffeeDrink; DrinkSize = Large } -> 12
-    | { DrinkType = TeaDrink; DrinkSize = Small } -> 6
-    | { DrinkType = TeaDrink; DrinkSize = Medium } -> 8
-    | { DrinkType = TeaDrink; DrinkSize = Large } -> 10
-    | { DrinkType = JuiceDrink; DrinkSize = Small } -> 10
-    | { DrinkType = JuiceDrink; DrinkSize = Medium } -> 12
-    | { DrinkType = JuiceDrink; DrinkSize = Large } -> 14
-    | _ -> failwith "Invalid drink size or type. Please, reenter your order."
+// Function to calculate the price of a product
+let calculatePrice (product: Product) = 
+    match product with
+    | Drink(drink, quantity) -> priceDrink(drink) * float quantity
 
+// Function to calculate the total price of a list with products
+let calculatePriceTotal (products: Product list) = 
+    let rec calcTotalRec pr acc = 
+        match pr with
+        | [] -> acc
+        | hd::tl -> (calculatePrice hd) + acc |> calcTotalRec tl
+    calcTotalRec products 0.0
+
+(*
+    This function is a recursive function that calculates the total price of a list with products. The calcTotalRec function is defined within the 
+    calculatePriceTotal function, which takes two arguments, pr and acc. pr is a list of products, and acc is the accumulated total price. 
+    It uses pattern matching to check if the list is empty or has elements. If the list is empty, it returns the accumulated total defaulted to 0.0. 
+    If there are elements, it adds the price of the first product to the accumulated total and calls the function recursively with the tail of the 
+    list. Finally, calculatePriceTotal calls calcTotalRec with the initial products list and an initial accumulated total of 0.0
+    *) 
